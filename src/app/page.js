a66d992da086d3ee91d1bd34fad3a7c36402f6ae
alie <mushyafaali@gmail.com>
2023-12-31 15:34:58 +0700

    style card

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,28 @@
-import AnimeList from "./components/AnimeList"
+import Card from "@/components/card"
+import Header from "@/utils/header"
+import Link from "next/link"
 
 const Home = async () => {
 
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`)
   
   const data = await response.json()
-  console.log(data)
+  // console.log(data)
   
   return (
-    <div>
-      <h1>Hello Next.js!</h1>
-      {data.data.map(data => {
+    <>
+    <Header/>
+    <div className="flex flex-wrap gap-3 justify-center bg-slate-100">
+      {data.data.slice(0, 4).map(data => {
+      // console.log(data)
         return (
-          <div key={data.mal_id}>
-            <AnimeList title = {data.title} images={data.images.webp.image_url}/>
-          </div>
+          <Link href={`/anime/${data.mal_id}`} key={data.mal_id}>
+            <Card title = {data.title} images={data.images.webp.image_url} year={data.year} rating={data.score}/>
+          </Link>
         )
       })}
     </div>
+    </>
   )
 }
 
